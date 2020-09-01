006e5011ed8bebb2e6500ad9dbcae631a17b18e8
David Collie <dmcollie@me.com>
2020-09-01 10:31:11 +0100

    Restart project with SwiftUI.

---
+++ Vital Stats/Vital Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vital Stats
+//
+//  Created by David Collie (Personal) on 01/09/2020.
+//  Copyright © 2020 David Collie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vital Stats/Vital StatsTests/Vital_StatsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Vital_StatsTests.swift
+//  Vital StatsTests
+//
+//  Created by David Collie (Personal) on 01/09/2020.
+//  Copyright © 2020 David Collie. All rights reserved.
+//
+
+import XCTest
+@testable import Vital_Stats
+
+class Vital_StatsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
